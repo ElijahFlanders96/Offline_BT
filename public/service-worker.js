16c1edcdc66a76275b030fef922735c239d7c2c8
Elijah Flanders <elijahflanders15@gmail.com>
2020-11-15 13:27:26 -0600

    wrote the index db file

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,7 +11,6 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-// install
 self.addEventListener("install", function(evt) {
     evt.waitUntil(
       caches.open(CACHE_NAME).then(cache => {
